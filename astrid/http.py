e4a00cdbabe0a6d1ff99da96e94bcda6bbf6af99
Parkayun <iamparkayun@gmail.com>
2015-08-08 11:32:28 +0900

    Added response

---
+++ astrid/http.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+__all__ = ('response',)
+
+import json
+
+from aiohttp.web import Response
+
+
+def response(body, content_type='text/plain'):
+    kwargs = {'body': body, 'content_type': content_type}
+
+    if isinstance(body, str):
+        kwargs['body'] = body.encode('utf-8')
+    elif isinstance(body, dict):
+        kwargs['content_type'] = 'application/json'
+        kwargs['body'] = json.dumps(body).encode('utf-8')
+    elif isinstance(body, int) or isinstance(body, float):
+        kwargs['body'] = str(body).encode('utf-8')
+
+    return Response(**kwargs)
