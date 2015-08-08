cafadc14545bccc3b5a49a721069fd4d8fe4621e
Parkayun <iamparkayun@gmail.com>
2015-08-08 11:37:39 +0900

    Fixed utf-8

--- astrid/http.py
+++ astrid/http.py
@@ -6,13 +6,13 @@ import json
 from aiohttp.web import Response
 
 
-def response(body, content_type='text/plain'):
+def response(body, content_type='text/plain; charset=utf-8'):
     kwargs = {'body': body, 'content_type': content_type}
 
     if isinstance(body, str):
         kwargs['body'] = body.encode('utf-8')
     elif isinstance(body, dict):
-        kwargs['content_type'] = 'application/json'
+        kwargs['content_type'] = 'application/json; charset=utf-8'
         kwargs['body'] = json.dumps(body).encode('utf-8')
     elif isinstance(body, int) or isinstance(body, float):
         kwargs['body'] = str(body).encode('utf-8')
