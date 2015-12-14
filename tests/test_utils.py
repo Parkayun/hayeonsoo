2eae92f5be7ef5162e277d23af219d75d3703d6b
Parkayun <iamparkayun@gmail.com>
2015-12-14 23:10:34 +0900

    Added test for MessageType

---
+++ tests/test_utils.py
@@ -0,0 +1,19 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from astrid.utils import MessageType
+
+
+def test_message_type():
+    test_cases = (
+        ('text', 0x1),
+        ('binary', 0x2),
+        ('ping', 0x9),
+        ('pong', 0xa),
+        ('close', 0x8),
+        ('closed', 20),
+        ('error', 21),
+    )
+    for case in test_cases:
+        attribute, expect_value = case
+        assert getattr(MessageType, attribute) == expect_value
