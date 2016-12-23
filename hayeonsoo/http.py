eb48e982a72cfe58f00feac714d2cbcb7b37e435
yun <yun@spoqa.com>
2016-12-23 10:48:07 +0900

    Sperate charset as a parameter

--- hayeonsoo/http.py
+++ hayeonsoo/http.py
@@ -15,15 +15,16 @@ def render(html, data={}):
     return response(body)
 
 
-def response(body, content_type='text/html; charset=utf-8', status_code=200):
-    kwargs = {'body': body, 'content_type': content_type, 'status': status_code}
+def response(body, charset='utf-8', content_type='text/html', status_code=200):
+    kwargs = {'body': body, 'charset': charset, 'content_type': content_type, 'status': status_code}
 
     if isinstance(body, str):
         kwargs['body'] = body.encode('utf-8')
     elif isinstance(body, dict):
-        kwargs['content_type'] = 'application/json; charset=utf-8'
+        kwargs['content_type'] = 'application/json'
         kwargs['body'] = json.dumps(body).encode('utf-8')
     elif isinstance(body, int) or isinstance(body, float):
         kwargs['body'] = str(body).encode('utf-8')
 
     return Response(**kwargs)
+
